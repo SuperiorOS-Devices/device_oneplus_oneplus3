218667d8b579b5746de74b94e1e8d989b4b6494d
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    op3: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_PACKAGES += \
     libcutils_shim \
     libui_shim
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
